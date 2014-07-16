f3d934d513c69bed838575aafa16a9e05a9d5c92
dovich <design@dovich.com.ua>
2014-07-16 15:42:13 +0300

    menu apdate, minor fixes

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+  $(document).ready(function(){
+
+    $(".navbar-toggle").click(function () {
+      
+      $(".navbar-header").hide();
+      $(".mobile-menu").show("slide", { direction: "right" }, 350);
+      
+      
+    });
+    
+$(".mob-collapse").click(function () {
+    $("body").removeClass("full");
+    $(".mobile-menu").hide("slide", { direction: "right" }, 350);
+    $(".navbar-header").show();
+  });
+    
+  });
